6bebcb69f3b0538ea78522b35006b724250961d7
Hetvi16-05 <103765741+Hetvi16-05@users.noreply.github.com>
2022-04-24 18:51:08 +0530

    Add files via upload

---
+++ data abstraction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class A
+{
+	private:
+	a=10
+	public:
+	void show()
+	{
+		cout<<a;
+	};
+};
+void main()
+{
+	clrscr();
+	int a=10;
+	A.ob;
+	ob.show();
+	getch();
+}
